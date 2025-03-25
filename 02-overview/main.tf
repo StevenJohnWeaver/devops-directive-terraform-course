4065414e06dbeefa05a78c9d7b3762dbe65cc274
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-03-25 11:11:25 -0400

    Update main.tf

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -7,6 +7,18 @@ terraform {
   }
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The AWS access key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS secret access key"
+  type        = string
+  sensitive   = true
+}
+
 # provider "aws" {
 #   region = "us-east-1"
 # }
